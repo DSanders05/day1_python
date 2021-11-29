984fd0261426fef4c63b982bb7e35708e7eb6596
Drake Sanders <drake_sanders12@icloud.com>
2021-11-29 16:15:13 -0600

    first commit

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+#1
+print("Hello World")
+
+#2a
+name = "Drake"
+print("Hello ", name)
+
+#2b
+name = "Drake"
+print("Hello " + name)
+
+#3a
+num = 21
+print("Hello", num)
+
+#3b
+num = 21
+print("Hello " + str(num))
+
+#4a
+food_1 = "steak"
+food_2 = "ice cream"
+print("I love to eat " + food_1 + " and " + food_2)
+
+#4b
+print(f"I love to eat {food_1} and {food_2}")
